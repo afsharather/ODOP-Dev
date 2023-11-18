5ddb3d5fc5527dd7e267a5eebb063bc42e4f17de
afsharather <eng21ct0001@dsu.edu.in>
2023-11-18 12:42:26 +0530

    SAST Security

---
+++ client/.eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "semi": ["error", "always"],
+        "no-unused-vars": "warn",
+        "camelcase": ["error", { "properties": "always" }],
+
+        "no-shadow": "error"
+
+
+    }
+};
